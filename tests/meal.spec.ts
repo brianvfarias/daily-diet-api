c569a2661c9811805f98a10cab230919608943a9
Brian Farias <brianvicthorandp@gmail.com>
2024-01-28 15:51:01 -0300

    Isolating test and testing the DB as well for POST /meal route

--- tests/meal.spec.ts
+++ tests/meal.spec.ts
@@ -1,4 +1,12 @@
-import { afterAll, beforeAll, it, describe, expect, beforeEach } from 'vitest';
+import {
+  afterAll,
+  beforeAll,
+  it,
+  describe,
+  expect,
+  beforeEach,
+  expectTypeOf,
+} from 'vitest';
 import supertest from 'supertest';
 import { app } from '../src/app';
 import { db } from '../src/database';
@@ -49,27 +57,57 @@ describe('Meal routes', () => {
       });
 
       expect(creationResponse.statusCode).toEqual(200);
-      cookies = creationResponse.headers['set-cookie'];
       expect(creationResponse.headers['set-cookie']).toSatisfy(properSessionID);
+      const selectCount = await db.select().count('*').from('meals').first();
+      expect(selectCount?.count).toEqual('1');
+
+      const createdRow = await db
+        .select('meal_name', 'meal_desc', 'belongs_to_diet')
+        .from('meals');
+      expect(createdRow).toEqual([
+        expect.objectContaining({
+          meal_name: 'Test 1',
+          meal_desc: 'Meal description for this test',
+          belongs_to_diet: true,
+        }),
+      ]);
     });
 
     it(`Should create meals that don't have "belongs_to_diet"`, async () => {
       const creationResponse = await supertest(app.server)
         .post('/meal')
-        .set('Cookie', [...cookies])
         .send({
           meal_name: 'Test 1',
           meal_desc: 'Meal description for this test',
-          // meal_time: '23 Jan 2023 12:55',
-          belongs_to_diet: true,
+          meal_time: '23 Jan 2023 12:55',
+          // belongs_to_diet: true,
         })
         .expect(200);
+      expect(creationResponse.headers['set-cookie']).toSatisfy(properSessionID);
+      const selectCount = await db.select().count('*').from('meals').first();
+      expect(selectCount?.count).toEqual('1');
+
+      const createdRow = await db
+        .select('meal_name', 'meal_desc', 'belongs_to_diet', 'meal_time')
+        .from('meals');
+      const rawTime = await db.raw(
+        `SELECT TO_TIMESTAMP(?, 'DD/MM/YYYY, HH24:MI:SS')`,
+        new Date('23 Jan 2023 12:55').toLocaleString('pt-BR')
+      );
+      const expectedTime = rawTime.rows[0].to_timestamp;
+      expect(createdRow).toEqual([
+        expect.objectContaining({
+          meal_name: 'Test 1',
+          meal_desc: 'Meal description for this test',
+          meal_time: expectedTime,
+          belongs_to_diet: true,
+        }),
+      ]);
     });
 
     it('Should create a new meal when all fields are passed', async () => {
       const creationResponse = await supertest(app.server)
         .post('/meal')
-        .set('Cookie', [...cookies])
         .send({
           meal_name: 'Test 1',
           meal_desc: 'Meal description for this test',
@@ -78,12 +116,43 @@ describe('Meal routes', () => {
         })
         .expect(200)
         .expect('New meal added!');
+
+      expect(creationResponse.headers['set-cookie']).toSatisfy(properSessionID);
+      const selectCount = await db.select().count('*').from('meals').first();
+      expect(selectCount?.count).toEqual('1');
+
+      const createdRow = await db
+        .select('meal_name', 'meal_desc', 'belongs_to_diet', 'meal_time')
+        .from('meals');
+
+      const rawTime = await db.raw(
+        `SELECT TO_TIMESTAMP(?, 'DD/MM/YYYY, HH24:MI:SS')`,
+        new Date('23 Jan 2023 12:55').toLocaleString('pt-BR')
+      );
+      const expectedTime = rawTime?.rows[0].to_timestamp;
+      expect(createdRow).toEqual([
+        expect.objectContaining({
+          meal_name: 'Test 1',
+          meal_desc: 'Meal description for this test',
+          meal_time: expectedTime,
+          belongs_to_diet: true,
+        }),
+      ]);
     });
   });
 
-  // describe('PUT /meal', () => {
-  //   it('should not update if there is no session_id', async () => {
-  //     const updateclient;
-  //   });
-  // });
+  describe('PUT /meal', () => {
+    it.todo('should update the created meal', async () => {
+      const creationResponse = await client.post('/meal').send({
+        meal_name: 'Test 1',
+        meal_desc: 'Meal description for this test',
+        meal_time: '23 Jan 2023 12:55',
+        belongs_to_diet: true,
+      });
+
+      if (creationResponse.statusCode === 200) {
+        const rowCreate = db.select('*').from('meals').first();
+      }
+    });
+  });
 });
